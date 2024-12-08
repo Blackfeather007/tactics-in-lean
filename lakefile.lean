c92c11792918e7da1d212fd23d6e6efed9e5d84a
timechess <tyc221@ruc.edu.cn>
2024-12-08 15:21:02 +0800

    delete doc-gen4 dependency

--- lakefile.lean
+++ lakefile.lean
@@ -13,7 +13,3 @@ lean_lib «TacticsInLean» where
   globs := #[.submodules `TacticsInLean]
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4"@"v4.14.0"
-
-
-meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "c7f4ac84b973b6efd8f24ba2b006cad1b32c9c53"
