55204c060ce97c64f58257c2a7d0971a898ab160
timechess <tyc221@ruc.edu.cn>
2024-12-10 23:27:47 +0800

    add documentation workflow

--- lakefile.lean
+++ lakefile.lean
@@ -13,3 +13,6 @@ lean_lib «TacticsInLean» where
   globs := #[.submodules `TacticsInLean]
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4"@"v4.14.0"
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "v4.14.0"
