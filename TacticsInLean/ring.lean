e08232138d0cfc7b0da88073d8d8b341b85b2f83
pelicanhere <fakegreenall@foxmail.com>
2024-12-20 18:38:13 +0800

    Update ring.lean

--- TacticsInLean/ring.lean
+++ TacticsInLean/ring.lean
@@ -1,5 +1,7 @@
 import Mathlib.tactic
 
+section Ring
+
 /-
 syntax "ring"... [Mathlib.Tactic.RingNF.ring]
   Tactic for evaluating expressions in *commutative* (semi)rings, allowing for variables in the
@@ -27,59 +29,39 @@ syntax "ring"... [Mathlib.Tactic.RingNF.ring]
 
 example {x y : ℤ} : (x + y) ^ 2 = x ^ 2 + 2 * x * y + y ^ 2 := by
   sorry
--- `ring` can be used to prove the equality in a commutative (semi)rings.
-
 
 example {n m : ℕ} : (n + m) ^ 3 = n ^ 3 + m ^ 3 + 3 * m ^ 2 * n + 3 * m * n ^ 2 := by
   sorry
--- It also works for natural numbers (commuative semiring).
 
-
-example (x y : ℝ) (f : ℝ → ℝ):  f (x + y) + f (y + x) = 2 * f (x + y) := by
+example (x y : ℝ) (f : ℝ → ℝ) : f (x + y) + f (y + x) = 2 * f (x + y) := by
   sorry
--- `ring_nf` can also prove some equations that ring may not be able to,
--- because they involve ring reasoning inside a subterm.
-
 
 example (x y : ℤ) (h : (x - y) ^ 2 = 0) : x ^ 2 + y ^ 2 = 2 * x * y := by
   sorry
--- `ring_nf` can be used at hypothesis.
 
-
-example (x y : ℕ) : x + id y = y + id x := by
+example (x y : ℕ) : x * 2 + id y = y + 2 * id x := by
   sorry
--- `ring!` will use a more aggressive reducibility setting to determine equality of atoms.
--- But it will not be always useful.
-
-example {R : Type} [Ring R] (a b : R) : (a + b) ^ 2 = a ^ 2 + b ^ 2 + a * b + b * a := by
-  ring says ring_nf -- nothing happened.
-  sorry
--- `ring` or `ring_nf` only work for commutative (semi)rings.
-
 
 example {R : Type} [Ring R] (a b : R) : (a + b) ^ 2 = a ^ 2 + b ^ 2 + a * b + b * a := by
   sorry
--- `noncomm_ring` can simplify identities in not-necessarily-commutative rings.
 
 
 open Complex
 
-example {a : ℝ} : a ^ 2 - 6 = (a + Real.sqrt 6) * (a - Real.sqrt 6) := by
+example {a : ℝ} : a ^ 2 - 6 = (a + Real.sqrt 6) * (a - Real.sqrt 6) :=
   sorry
 
-example (x : ℝ) (hx : x ^ 2 - 5 * x + 6 = 0) :  x = 3 ∨ x = 2 := by
+example (x : ℝ) (hx : x ^ 2 - 5 * x + 6 = 0) :  x = 3 ∨ x = 2 :=
   sorry
 
-
-example (a : ℂ) (h : (a + 2) ^ 2 = - 9) : a + 2 = 3 * I ∨ a + 2 = - (3 * I) := by
-  sorry
--- Try using `ring` to simpilify each small step.
-
-
-example (a b : ℝ) : 2 * a * b ≤ a ^ 2 + b ^ 2 := by
+example (a : ℂ) (h : (a + 2) ^ 2 = - 9) : a + 2 = 3 * I ∨ a + 2 = - (3 * I) :=
   sorry
 
-
+example (a b : ℝ) : 2 * a * b ≤ a ^ 2 + b ^ 2 :=
+  sorry
 
 theorem Cauchy_Schwarz_Ineq (a b c d : ℝ) :
-    a * c + b * d ≤ (a ^ 2 + b ^ 2) * (c ^ 2 + d ^ 2) := sorry
+    (a * c + b * d) ^ 2 ≤ (a ^ 2 + b ^ 2) * (c ^ 2 + d ^ 2) :=
+  sorry
+
+end Ring
