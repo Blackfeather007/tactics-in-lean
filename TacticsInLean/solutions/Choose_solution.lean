e3e35acd9cf6a050ba2df506638ef93f731bb1cf
Yi.Yuan <kysyy1@126.com>
2024-12-25 12:00:29 +0800

    yy-choose (#5)
    * Update Choose_solution.lean
    
    * Update Choose_solution.lean
    
    * Update Choose_solution.lean
    
    * Update Choose_solution.lean
    
    * Update Choose_solution.lean
    
    * Update Choose_solution.lean
    
    * Update Choose_solution.lean
    
    * Update Choose_solution.lean
    
    * Update Choose_solution.lean
    
    * Update Choose_solution.lean
    
    * Create Choose.lean
    
    * Update Choose.lean
    
    * Update Choose.lean
    
    * Update Choose.lean
    
    * simp Example2
    
    * simp
    
    * revise
    
    * Update Choose.lean
    
    * Update Choose.lean
    
    * Update Choose.lean

--- TacticsInLean/solutions/Choose_solution.lean
+++ TacticsInLean/solutions/Choose_solution.lean
@@ -1,90 +1,56 @@
-/-编写者 : 袁奕(Yuan Yi), 其中`橘色`字体表示面向助教老师们的注释
--/
-import Mathlib
+import Mathlib.Tactic
+
 
 open Classical
 suppress_compilation -- because everything is noncomputable
 
+#help tactic choose
 
+example (h : ∀ n m : ℕ, ∃ i j, m = n + i ∨ m + j = n) : True := by
+    choose i j h using h
+    guard_hyp i : ℕ → ℕ → ℕ
+    guard_hyp j : ℕ → ℕ → ℕ
+    guard_hyp h : ∀ (n m : ℕ), m = n + i n m ∨ m + j n m = n
+    trivial
+
+example (h : ∀ i : ℕ, i < 7 → ∃ j, i < j ∧ j < i+i) : True := by
+  choose! f h h' using h
+  guard_hyp f : ℕ → ℕ
+  guard_hyp h : ∀ (i : ℕ), i < 7 → i < f i
+  guard_hyp h' : ∀ (i : ℕ), i < 7 → f i < i + i
+  trivial
 
 section Introduction1
---Reference : https://www.ma.imperial.ac.uk/~buzzard/xena/formalising-mathematics-2024/Part_B/the_axiom_of_choice.html
-/- In my experience, the way people want to use the axiom of choice when doing mathematics in Lean
- is to get an element of X not from a hypothesis ∃ x : X, true, but from a hypothesis like
- ∃ x : ℝ, x^2 = 2 or more generally ∃ x : X, p x where p : X → Prop is a predicate on X. The way to
- do this is as follows: you run Classical.choose on h : ∃ x : X, p x to get the element of X, and
- the proof that this element satisfies p is Classical.choose_spec h. Here’s a worked example.-/
-
-open Polynomial -- so I can use notation ℂ[X] for polynomial rings
-                -- and so I can write `X` and not `polynomial.X`
-
-def f : ℂ[X] := X^5 + X + 37 -- a random polynomial
-
-lemma f_degree : degree f = 5 := by
-  unfold f
-  compute_degree -- polynomial degree computing tactic
-  · norm_num
-  · exact Nat.le_of_ble_eq_true rfl
-
-theorem f_has_a_root : ∃ (z : ℂ), f.IsRoot z := by
-  apply Complex.exists_root -- the fundamental theorem of algebra
-  -- ⊢ 0 < degree f
-  rw [f_degree]
-  -- ⊢ 0 < 5
-  norm_num
-
--- let z be a root of f (getting data from a theorem)
-def z : ℂ := choose f_has_a_root
-
--- proof that z is a root of f (the "API" for `Classical.choose`)
-theorem z_is_a_root_of_f : f.IsRoot z := by exact choose_spec f_has_a_root
-
--- #check choose f_has_a_root
--- #check choose_spec f_has_a_root
-
---We should prioritize using **recases** or **obtain** over **Classcal.choose**.
-example : 1 = 1 := by
-  rcases f_has_a_root with  ⟨z1, z1_is_a_root_of_f⟩
-  -- #check z1
-  -- #check z1_is_a_root_of_f
-  have z1_is_a_root_of_f : f.IsRoot z1 := by exact z1_is_a_root_of_f
-
-  simp only
-
-example : 1 = 1 := by
-  obtain ⟨z2, z2_is_a_root_of_f⟩ := f_has_a_root
-  -- #check z2
-  -- #check z2_is_a_root_of_f
-  have z2_is_a_root_of_f : f.IsRoot z2 := by exact z2_is_a_root_of_f
-
-  simp only
-
-end Introduction1
-
-
-
-section Introduction3
 --Reference : https://www.ma.imperial.ac.uk/~buzzard/xena/formalising-mathematics-2024/Part_C/tactics/choose.html
-/- Summary : The choose tactic is a relatively straightforward way to go from a proof of a
- proposition of the form ∀ x, ∃ y, P(x,y) (where P(x,y) is some true-false statement depend on x
- and y), to an actual function which inputs an x and outputs a y such that P(x,y) is true.
+--Reference : https://github.com/leanprover-community/mathlib4/blob/725fd7ac1c92cf4d7984b25646052f5a6e717d31/Mathlib/Tactic/Choose.lean#
+/-
+ **∀ x, ∃ y, P(x,y) (where P(x,y) is some true-false statement depend on x and y)**
+ **to an actual function which inputs an x and outputs a y such that P(x,y) is true.**
+ * `choose a b h h' using hyp` takes a hypothesis `hyp` of the form
+  `∀ (x : X) (y : Y), ∃ (a : A) (b : B), P x y a b ∧ Q x y a b`
+  for some `P Q : X → Y → A → B → Prop` and outputs
+  into context a function `a : X → Y → A`, `b : X → Y → B` and two assumptions:
+  `h : ∀ (x : X) (y : Y), P x y (a x y) (b x y)` and
+  `h' : ∀ (x : X) (y : Y), Q x y (a x y) (b x y)`. It also works with dependent versions.
 
-  Basic usage : The simplest situation where you find yourself wanting to use choose is if you have
- a function f : X → Y which you know is surjective, and you want to write down a one-sided inverse
- g : Y → X, i.e., such that f(g(y))=y for all y : Y. Here’s the set-up:-/
+* `choose! a b h h' using hyp` does the same, except that it will remove dependency of
+  the functions on propositional arguments if possible. For example if `Y` is a proposition
+  and `A` and `B` are nonempty in the above example then we will instead get
+  `a : X → A`, `b : X → B`, and the assumptions
+  `h : ∀ (x : X) (y : Y), P x y (a x) (b x)` and
+  `h' : ∀ (x : X) (y : Y), Q x y (a x) (b x)`.
 
-/-`X` is an abstract type and `P` is an abstract true-false
-statement depending on an element of `X` and a real number.-/
+The `using hyp` part can be omitted,
+which will effectively cause `choose` to start with an `intro hyp`.
+-/
 example (X : Type) (P : X → ℝ → Prop)
-    /-
-    `h` is the hypothesis that given some `ε > 0` you can find
-    an `x` such that the proposition is true for `x` and `ε`
-    -/
-    (h : ∀ ε > 0, ∃ x, P x ε) :
-  /-
-  Conclusion: there's a sequence of elements of `X` satisfying the
-  condition for smaller and smaller ε
-  -/
+/-
+`h` is the hypothesis that given some `ε > 0` you can find an `x` such that the proposition is true for `x` and `ε`
+-/
+(h : ∀ ε > 0, ∃ x, P x ε) :
+/-
+Conclusion: there's a sequence of elements of `X` satisfying the condition for smaller and smaller ε
+-/
   ∃ u : ℕ → X, ∀ n, P (u n) (1/(n+1)) := by
   choose g hg using h
   /-
@@ -97,24 +63,26 @@ example (X : Type) (P : X → ℝ → Prop)
   intro n
   apply hg
 
-end Introduction3
+end Introduction1
 
 
 
 section Introduction2
 --Reference : Mathmatics in Lean C4S2
-/- To define the inverse of a function f : α → β, we will use two new ingredients. First, we need to
- deal with the fact that an arbitrary type in Lean may be empty. To define the inverse to f at y
- when there is no x satisfying f x = y, we want to assign a default value in α. Adding the
- annotation [Inhabited α] as a variable is tantamount to assuming that α has a preferred element,
- which is denoted default. Second, in the case where there is more than one x such that f x = y, the
- inverse function needs to choose one of them. This requires an appeal to the axiom of choice. Lean
- allows various ways of accessing it; one convenient method is to use the classical choose operator,
-  illustrated below.
--/
+--Sometimes the **recases and obtain may not work**
+----`主讲老师请务必强调, 能用rcases 或 obtain 解决就不要用 choose`
+
 variable {α β : Type*} [Inhabited α]
 
 #check (default : α)
+/-
+ Inhabited α is a typeclass that says that α has a designated element, called (default : α).
+ This is sometimes referred to as a "pointed type".
+ This class is used by functions that need to return a value of the type when called "out of domain".
+ For example, Array.get! arr i : α returns a value of type α when arr : Array α, but if i is not in range of the array,
+ it reports a panic message, but this does not halt the program, so it must still return a value of type α
+ (and in fact this is required for logical consistency), so in this case it returns default.
+-/
 
 variable (P : α → Prop) (h : ∃ x, P x)
 
@@ -123,9 +91,8 @@ variable (P : α → Prop) (h : ∃ x, P x)
 example : P (Classical.choose h) :=
   Classical.choose_spec h
 
-/- Given h : ∃ x, P x, the value of Classical.choose h is some x satisfying P x. The theorem
-Classical.choose_spec h says that Classical.choose h meets this specification. With these in hand,
-we can define the inverse function as follows: -/
+/- **Given prop h : ∃ x, P x, the value of Classical.choose h is some x satisfying P x.**
+ **The theorem Classical.choose_spec h says that Classical.choose h meets this specification.**-/
 
 def inverse (f : α → β) : β → α := fun y : β ↦
   if h : ∃ x, f x = y then Classical.choose h else default
@@ -134,26 +101,130 @@ theorem inverse_spec {f : α → β} (y : β) (h : ∃ x, f x = y) : f (inverse 
   rw [inverse, dif_pos h]
   exact Classical.choose_spec h
 
-/-The lines noncomputable section and open Classical are needed because we are using classical logic
- in an essential way. On input y, the function inverse f returns some value of x satisfying f x = y
- if there is one, and a default element of α otherwise. This is an instance of a dependent if
- construction, since in the positive case, the value returned, Classical.choose h, depends on the
- assumption h. The identity dif_pos h rewrites if h : e then a else bto a given h : e, and,
- similarly, dif_neg h rewrites it to b given h : ¬ e. There are also versions if_pos and if_neg that
- works for non-dependent if constructions and will be used in the next section. The theorem
- inverse_spec says that inverse f meets the first part of this specification.-/
-
 end Introduction2
 
 
 
 section Example1
+--Reference : https://github.com/leanprover-community/mathlib4/blob/b09464fc7b0ff4bcfd4de7ff54289799009b5913/Mathlib/Logic/Equiv/Set.lean#L406
+--`有类型转换`
 
---Sometimes the **recases and obtain may not work**
+open Set
+
+#check Equiv.Set.imageOfInjOn
+
+/-- If a function `f` is injective on a set `s`, then `s` is equivalent to `f '' s`. -/
+def myimageOfInjOn {α β} (f : α → β) (s : Set α) (H : InjOn f s) : s ≃ f '' s where
+  toFun := by
+    intro p
+    use f p
+    exact mem_image_of_mem f p.2
+  invFun := by
+    intro p
+    --`挖空`
+    have hint : p.1 ∈ f '' s := p.2
+    use choose p.2
+    exact (choose_spec p.2).1
+  left_inv := by
+    --We want to prove f⁻¹ (f p) = p
+    intro p
+    have : f p ∈ f '' s := mem_image_of_mem f p.2
+    show ⟨choose this, (choose_spec this).1⟩ = p
+
+    have feq : f (choose this) = f p := by
+      --`挖空`
+      exact (choose_spec (mem_image_of_mem f p.2)).2
+
+    have : (choose this) =  p := by
+      apply H
+      --`挖空`
+      exact (choose_spec (mem_image_of_mem f p.2)).1
+      exact p.2
+      exact feq
+    exact SetCoe.ext this
+
+  right_inv := by
+    --We want to prove f (f⁻¹ p) = p
+    intro p
+    have : f (choose p.2) = p.1 := by
+      --`挖空`
+      exact (choose_spec p.2).2
+
+    exact SetCoe.ext this
+end Example1
+
+
+
+section Example2
+
+open Set
+
+theorem mySet.InjOn.image_iInter_eq{α : Type*} {β : Type*} {ι : Sort*} [Nonempty ι] [Inhabited ι]{s : ι → Set α} {f : α → β}
+ (h : Set.InjOn f (⋃ (i : ι), s i)) : f '' ⋂ (i : ι), s i ⊇ ⋂ (i : ι), f '' s i := by
+  intro y hy
+  simp only [mem_iInter, mem_image] at hy
+  --`挖空`
+  choose x hx hy using hy
+  use x default
+
+  constructor
+  · apply mem_iInter.2
+    intro i
+    suffices x default = x i by
+      rw [this]
+      apply hx
+    have : ∀ i, x i ∈ ⋃ j, s j := fun i => (subset_iUnion _ _) (hx i)
+    apply h (this _) (this _)
+    simp only [hy]
+  · exact hy default
+
+end Example2
+
+
+
+section Exercise1
+--Reference : https://github.com/leanprover-community/mathlib4/blob/c91dd0e151d3f0b6755d35119cd7943a516addb8/Mathlib/Data/Set/Function.lean#L669-L680
+
+open Set
+
+#check exists_eq_graphOn_image_fst
+
+theorem myexists_eq_graphOn_image_fst{α : Type*} {β : Type*} [Nonempty β] {s : Set (α × β)}
+(h : Set.InjOn Prod.fst s) : ∃ (f : α → β), s = Set.graphOn f (Prod.fst '' s):= by
+  have : ∀ x ∈ Prod.fst '' s, ∃ y, (x, y) ∈ s := forall_mem_image.2 fun (x, y) h ↦ ⟨y, h⟩
+  --`挖空`
+  choose! f hf using this
+
+  rw [forall_mem_image] at hf
+  use f
+  rw [graphOn, image_image, EqOn.image_eq_self]
+  exact fun x hx ↦ h (hf hx) hx rfl
+
+end Exercise1
+
+
+
+section Exercise2
+--Reference : https://github.com/leanprover-community/mathlib4/blob/c91dd0e151d3f0b6755d35119cd7943a516addb8/Mathlib/Data/Set/Lattice.lean#L198-L201
+
+open Set
+
+#check nonempty_of_nonempty_iUnion
+
+theorem mynonempty_of_nonempty_iUnion {α : Type*} {ι : Sort*} {s : ι → Set α} (h_Union : (⋃ i, s i).Nonempty) :
+ Nonempty ι := by
+  obtain ⟨x, hx⟩ := h_Union
+  have : ∃ i, x ∈ s i := mem_iUnion.mp hx
+  use Classical.choose this
+
+end Exercise2
+
+
+
+section Exercise3
+
 open Pointwise
-/-**Example 1** : backgroud(this project will be one of the finnal projet) :
-Suppose that $G$ is a group with subgroups $H$ and $K$ such that $H \cap K = \{1\}$, then
-$HK \cong H \times K$ (as sets).-/
+
 def Hom_top_product_of_normal_of_disjoint (H K : Set ℝ) : (H * K) → (H × K) := by
   intro x
 
@@ -163,7 +234,7 @@ def Hom_top_product_of_normal_of_disjoint (H K : Set ℝ) : (H * K) → (H × K)
 
   --`此题涉及到若干处类型转换, 较为困难,因此主讲老师 必须 讲解这道题`
   --之所以考虑保留此题是考虑到类型转换"早折磨晚折磨应该也差不多"
-  --`主讲老师请务必强调, 能用rcases 或 obtain 解决就不要用 choose`
+
   -- obtain⟨a, ha⟩ := mem_mul_eq
   set a := choose mem_mul_eq with a_def
   set ha := choose_spec mem_mul_eq
@@ -175,68 +246,64 @@ def Hom_top_product_of_normal_of_disjoint (H K : Set ℝ) : (H * K) → (H × K)
   use ⟨a, ha.1⟩, b
   exact hb.1
 
-end Example1
+end Exercise3
 
 
 
-section Exercise1
+section Exercise4
+--Reference : https://github.com/leanprover-community/mathlib4/blob/c91dd0e151d3f0b6755d35119cd7943a516addb8/Mathlib/Data/Set/Lattice.lean#L1942-L1949
 
---Reference : https://github.com/leanprover-community/mathlib4/blob/b09464fc7b0ff4bcfd4de7ff54289799009b5913/Mathlib/Logic/Equiv/Set.lean#L406
-universe u v w z
-variable {α : Sort u} {β : Sort v} {γ : Sort w}
+#check Set.sigmaEquiv
 
-open Set
+noncomputable def mySet.sigmaEquiv{α : Type*} {β : Type*} (s : α → Set β) (hs : ∀ (b : β), ∃! i : α, b ∈ s i) :
+(i : α) × (s i) ≃ β where
+  toFun := by
+    intro p
+    use p.2
+  invFun := by
+    intro b
+    exact ⟨(hs b).choose, ⟨b, (hs b).choose_spec.1⟩⟩
+  left_inv := by
+    intro p
+    ext
+    · show (hs p.2).choose = p.1
+      apply ExistsUnique.unique (hs p.2)
+      · exact (hs p.2).choose_spec.1
+      · exact Subtype.coe_prop p.snd
+    · rfl
+  right_inv _:= rfl
 
-/-- If a function `f` is injective on a set `s`, then `s` is equivalent to `f '' s`. -/
-def imageOfInjOn1 {α β} (f : α → β) (s : Set α) (H : InjOn f s) : s ≃ f '' s where
-  toFun := fun p => ⟨f p, mem_image_of_mem f p.2⟩
-  invFun := fun p => ⟨Classical.choose p.2, (choose_spec p.2).1⟩
-  left_inv := fun ⟨_, h⟩ => Subtype.eq
-      (H (choose_spec (mem_image_of_mem f h)).1 h
-        (choose_spec (mem_image_of_mem f h)).2)
-  right_inv :=  fun ⟨_, h⟩ => Subtype.eq (Classical.choose_spec h).2
-
-end Exercise1
-
-
-
--- section Exercise2
-
--- open Polynomial
--- --https://github.com/leanprover-community/mathlib4/blob/1ed7634f46ba697f891ebfb3577230329d4b7196/Mathlib/Algebra/Polynomial/Coeff.lean#L319
-
--- #check C_dvd_iff_dvd_coeff
--- theorem my_Polynomial.C_dvd_iff_dvd_coeff{R : Type u} [Semiring R] (r : R) (φ : Polynomial R)(h : ∀ i, r ∣ φ.coeff i) :
---  C r ∣ φ := by
---   have : ∃ c : ℕ → R, ∀ (i : ℕ), φ.coeff i = r * c i := by
-
---     sorry
-
-
---   choose c hc using h
---   let c' : ℕ → R := fun i => if i ∈ φ.support then c i else 0
---   let ψ : R[X] := ∑ i ∈ φ.support, monomial i (c' i)
---   use ψ
---   ext i
---   simp only [c', ψ, coeff_C_mul, mem_support_iff, coeff_monomial, finset_sum_coeff,
---     Finset.sum_ite_eq']
---   split_ifs with hi
---   · rw [hc]
---   · rw [Classical.not_not] at hi
---     rwa [mul_zero]
-
--- end Exercise2
+end Exercise4
 
 
 
-section Exercise3
+section Exercise5
 --Some thing useful which similar to Classical.choose
+--Reference : https://github.com/leanprover-community/mathlib4/blob/c91dd0e151d3f0b6755d35119cd7943a516addb8/Mathlib/Algebra/Order/Archimedean/Basic.lean#L225-L236
 
 #check Nat.find
 #check Nat.find_spec
 #check Nat.find_min
 
---https://github.com/leanprover-community/mathlib4/blob/1ed7634f46ba697f891ebfb3577230329d4b7196/Mathlib/Algebra/Order/CauSeq/BigOperators.lean#L154
+#check exists_nat_pow_near
+
+theorem myexists_nat_pow_near {x y : ℕ}(hx : 1 ≤ x) (hy : 1 < y) : ∃ n : ℕ, y ^ n ≤ x ∧ x < y ^ (n + 1) := by
+  have h : ∃ n : ℕ, x < y ^ n := pow_unbounded_of_one_lt _ hy
+  let n := Nat.find h
+  have hn : x < y ^ n := Nat.find_spec h
+  have hnp : 0 < n :=
+    pos_iff_ne_zero.2 fun hn0 => by rw [hn0, pow_zero] at hn; exact not_le_of_gt hn hx
+  have hnsp : Nat.pred n + 1 = n := Nat.succ_pred_eq_of_pos hnp
+  have hltn : Nat.pred n < n := Nat.pred_lt (ne_of_gt hnp)
+  exact ⟨Nat.pred n, le_of_not_lt (Nat.find_min h hltn), by rwa [hnsp]⟩
+
+end Exercise5
+
+
+
+section Exercise6
+--Reference : https://github.com/leanprover-community/mathlib4/blob/1ed7634f46ba697f891ebfb3577230329d4b7196/Mathlib/Algebra/Order/CauSeq/BigOperators.lean#L154
+
 #check IsCauSeq.of_decreasing_bounded
 
 theorem my_IsCauSeq.of_decreasing_bounded (f : ℕ → ℝ) {a : ℝ}(ham : ∀ n, |f n| ≤ a) (hnm : ∀ n, f n.succ ≤ f n) :
@@ -284,11 +351,4 @@ IsCauSeq abs f := fun ε ε0 ↦ by
         rw [← Nat.succ_pred_eq_of_pos (Nat.pos_of_ne_zero hl0), succ_nsmul, sub_add, add_sub_cancel_right]
     _ < f j + ε := by linarith[hl j]
 
-end Exercise3
-
-
---Reference : https://github.com/leanprover-community/mathlib4/blob/197eebfa5455aa09b7a4c2ad3c3eb9123245df90/Mathlib/FieldTheory/JacobsonNoether.lean#L120
-
-
-
--- example [R : Type*][Ring R](ι : ℕ → Ideal R) : ∀ i, ι i ≤ ι (i + 1) → ∃ k, ∀ i > k , ι i = ι k := sorry
+end Exercise6
